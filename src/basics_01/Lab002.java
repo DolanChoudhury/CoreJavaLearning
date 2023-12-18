d41fb469a3c6d0d6eda356fff1cb9b1cdcd271e4
Dolan <riyachoudhury42@gmail.com>
2023-12-18 11:23:30 +0530

    My first commit on 18th Dec

---
+++ src/basics_01/Lab002.java
@@ -0,0 +1,25 @@
+package src.basics_01;
+
+public class Lab002 {
+
+        public static void main(String[] args) {
+
+            int a;
+            a=150;
+            System.out.println("The required number is " +a);
+
+            System.out.println(a+1);
+
+            byte b= 127;
+            System.out.println(b);
+            System.out.println(b+1);
+
+        /*
+        b=b+1;
+        System.out.println(b);
+       */
+
+
+        }
+
+}
